437c5e803577789f58235e91f6193ca5b516508c
Vijay Pal Singh Kadela <vinaypalsingh085@gmail.com>
2024-06-12 19:48:38 +0530

    next commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import About from "./components/About";
 import Cards from "./components/Cards";
 import Eyes from "./components/Eyes";
 import Featured from "./components/Featured";
+import Footer from "./components/Footer";
 import LandingPage from "./components/LandingPage";
 import Marquee from "./components/Marquee";
 import Navber from "./components/Navber";
@@ -16,6 +17,7 @@ function App() {
       <Eyes/>
       <Featured/>
       <Cards/>
+      <Footer/>
     </div>
   );
 }
